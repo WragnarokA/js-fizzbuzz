4fc449651fb9e3606ded8d763d5715e67147c4ce
WragnarokA <wilmer.arango.a@gmail.com>
2023-04-18 03:56:20 +0200

    lista solo console

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+// Per i multipli di 3 stampi “Fizz” al posto del numero
+// Per i multipli di 5 stampi “Buzz”
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+
+    } else if(i % 3 === 0) {
+      console.log("Fizz");
+    
+    }  else if(i % 5 === 0) {
+      console.log("Buzz");
+
+    }  else{
+        console.log(i);
+
+    } 
+        
+}
+      
+
+
+
+
+
+// for (let lista = 1; lista <= 100 ; lista++) {
+    
+//     console.log(lista);
+//     fizz = lista * 3;
+//     buzz = lista *5;
+//     fizzBuzz = lista * 15;
+
+//     if (lista = fizz) {
+        
+//         console.log("Fizz");
+        
+//     } else {
+        
+//     }
+    
+// }
